0b05a26433e6ea11636c7d2c45bd2558828b5d12
NLmeng <60766732+NLmeng@users.noreply.github.com>
2022-08-28 03:33:39 -0700

    Add files via upload

--- src_code.js
+++ src_code.js
@@ -11,15 +11,24 @@ setTimeout(function() {
     document.getElementById("my_ocp1").style.filter = "blur(0px)";
 }, 3000);
 // 
+
 //
 document.querySelector("#arrow1").addEventListener('click', function () {
     document.querySelector("#btn1").click();
+    // document.getElementById("div1").style.height       = "100vh";
+    // document.getElementById("my_name1").style.fontSize = "2.5rem";
+    // document.getElementById("my_ocp1").style.fontSize  = "2rem";
+    // document.getElementById("par1").style.fontSize     = "1.5rem";
 });
 
 document.querySelector("#arrow2").addEventListener('click', function () {
     document.querySelector("#btn2").click();
 });
 
+document.querySelector("#arrow3").addEventListener('click', function () {
+    document.querySelector("#btn3").click();
+});
+//
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
