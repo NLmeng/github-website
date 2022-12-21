28ecd25361f3ab62c6e3f8b4933e39b476746c2d
Lymeng Naret <lymengnaret@Lymengs-MacBook-Air.local>
2022-12-21 00:20:49 -0800

    added Reactjs buttons

---
+++ curr/buttons.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const e = React.createElement;
+const domContainer = document.querySelector('#home_button');
+
+class HomeButton extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { 
+      clicked: false,
+      hovered: false,
+    };
+  }
+
+  render() {
+    if (this.state.clicked) {
+      return 'clicked';
+    }
+
+    const buttonStyles = {
+      border: '1px solid black',
+      padding: '5px 20px 5px 20px',
+      borderRadius: '20px',
+      transform: this.state.hovered ? 'scale(1.05)' : 'scale(1)',
+      transition: 'transform 0.2s ease-in-out',
+      backgroundColor: this.state.hovered ? 'rgba(115,139,230,0.7)' : 'white'
+    }
+
+    return e(
+      'button',
+      { 
+        onClick:      () => this.setState({ clicked: true }),
+        onMouseEnter: () => this.setState({ hovered: true }),
+        onMouseLeave: () => this.setState({ hovered: false }),
+        style: buttonStyles,
+      },
+      'Home'
+    );
+  }
+
+}
+
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(HomeButton));
