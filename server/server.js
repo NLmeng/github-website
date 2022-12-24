39924ce1bea74e113616383a717f25c67f593f70
Lymeng Naret <lymengnaret@Lymengs-MacBook-Air.local>
2022-12-23 16:24:22 -0800

    structured header, body, footer

--- server/server.js
+++
@@ -1,5 +0,0 @@
-// const express = require('express')
-
-export default class Server {
-
-}
--- src/src.js
+++ src/src.js
@@ -1,15 +1,12 @@
 import {HomeButton} from './home_buttons.js';
 import {CreateDiv} from './create_div.js';
-import Server from '../server/server.js'
-
 //
 const home_button = React.createElement;
 const domContainer_home_button = document.querySelector('#home_button');
 const root = ReactDOM.createRoot(domContainer_home_button);
 root.render(home_button(HomeButton));
 //
-const cd = React.createElement;
-const domContainer_cd = document.querySelector('#create-div');
-ReactDOM.createRoot(domContainer_cd).render(cd(CreateDiv));
+// const cd = React.createElement;
+// const domContainer_cd = document.querySelector('#create-div');
+// ReactDOM.createRoot(domContainer_cd).render(cd(CreateDiv));
 //
-const server = new Server();
